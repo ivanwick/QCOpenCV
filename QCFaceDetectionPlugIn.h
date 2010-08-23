fb16cce403c69e6300b0da0f46280a9c9374a547
Ivan Wick <ivan@slab.local>
2010-08-23 00:19:03 -0400

    Empty Quartz Composer plugin project

---
+++ QCFaceDetectionPlugIn.h
@@ -0,0 +1,22 @@
+//
+//  QCFaceDetectionPlugIn.h
+//  QCFaceDetection
+//
+//  Created by Ivan Wick on 8/23/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface QCFaceDetectionPlugIn : QCPlugIn
+{
+}
+
+/*
+Declare here the Obj-C 2.0 properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property(assign) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
